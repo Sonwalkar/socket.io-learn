db0d45463f4e718fb573b0dd1058afcc1adca336
Akshay Sonwalkar <sonakashay@gmail.com>
2024-01-14 18:41:40 +0530

    feat: first commit

---
+++ app/app.js
@@ -0,0 +1,112 @@
+const socket = io("ws://localhost:5002");
+
+socket.on('connect', () => {
+  const el = document.createElement('li');
+  el.innerHTML = `<b>Your Id - ${socket.id}`;
+  document.querySelector("ul").appendChild(el);
+})
+
+socket.on('clientList', (clients) => {
+  console.log("clients: ", clients);
+
+  const inactiveDivs = document.querySelectorAll(".window .userList > div.inactive");
+  for (const div of inactiveDivs) {
+    div.remove();
+    console.log("removed inactive users");
+  }
+
+  for (const client of clients) {
+    const div = document.createElement("div");
+    div.className = "inactive";
+    div.innerText = client.clientName;
+    div.dataset.value = client.socketId;
+    document.querySelector(".window .userList").appendChild(div);
+  }
+})
+
+
+socket.on("message", text => {
+  console.log("message", text);
+
+  if (document.querySelector(".window .userList .active").dataset.value === text.socketId) {
+    if (text.socketId === socket.id) {
+      const fromOtherDiv = document.createElement("div");
+      fromOtherDiv.className = "fromYou";
+      const div = document.createElement("div");
+      div.innerText = text.message;
+      fromOtherDiv.appendChild(div);
+      document.querySelector(".window .chat .activeChat").appendChild(fromOtherDiv);
+    } else {
+      const fromOtherDiv = document.createElement("div");
+      fromOtherDiv.className = "fromOther";
+      const div = document.createElement("div");
+      div.innerText = text.message;
+      fromOtherDiv.appendChild(div);
+      document.querySelector(".window .chat .activeChat").appendChild(fromOtherDiv);
+    }
+  }
+
+})
+
+// it selects client(socketId) to make active and inactive look
+document.querySelector(".window .userList").addEventListener("click", (e) => {
+  const activeDiv = document.querySelector(".window .userList .active");
+  if (activeDiv) {
+    activeDiv.classList.remove("active");
+    activeDiv.classList.add("inactive");
+  }
+  e.target.classList.add("active");
+  e.target.classList.remove("inactive");
+
+  const otherClientId = document.querySelector(".window .userList .active").dataset.value;
+
+  socket.emit("activeRoomSwitch", socket.id, otherClientId)
+})
+
+socket.on("activeRoomChatOnSwitch", chatHistory => {
+  console.log("chatHistory: ", chatHistory);
+
+  const activeChatDiv = document.querySelector(".window .chat .activeChat");
+
+  for (const chat of chatHistory) {
+    if (chat.from === socket.id) {
+      const fromOtherDiv = document.createElement("div");
+      fromOtherDiv.className = "fromYou";
+      const div = document.createElement("div");
+      div.innerText = chat.message;
+      fromOtherDiv.appendChild(div);
+      activeChatDiv.appendChild(fromOtherDiv);
+    } else {
+      const fromOtherDiv = document.createElement("div");
+      fromOtherDiv.className = "fromOther";
+      const div = document.createElement("div");
+      div.innerText = chat.message;
+      fromOtherDiv.appendChild(div);
+      activeChatDiv.appendChild(fromOtherDiv);
+    }
+  }
+});
+
+document.querySelector("#send").addEventListener("click", () => {
+  const text = document.querySelector("#message").value;
+  const room = document.querySelector(".window .userList .active").dataset.value;
+
+  // add message to chat window as sender
+  const fromOtherDiv = document.createElement("div");
+  fromOtherDiv.className = "fromYou";
+  const div = document.createElement("div");
+  div.innerText = text;
+  fromOtherDiv.appendChild(div);
+  document.querySelector(".window .chat .activeChat").appendChild(fromOtherDiv);
+
+
+  console.log("ele", document.querySelector(".window .userList .active"));
+  console.log("room: ", room);
+  socket.emit("message", text, room);
+  document.querySelector("#message").value = "";
+})
+
+document.querySelector("#join").addEventListener("click", () => {
+  const room = document.querySelector("#room").value;
+  socket.emit("join-room", room);
+});
---
+++ server/index.js
@@ -0,0 +1,71 @@
+import { Server as serverSocketIo } from "socket.io";
+import http from "http";
+
+const server = http.createServer();
+
+const io = new serverSocketIo(server, {
+  cors: { origin: "*" }
+});
+
+const clients = [];
+let chatPerClient = {}; //  { <client_id>to<client_id>: [{ from: "", message: ""}]}
+
+io.on("connection", (socket) => {
+  console.log("connection established");
+
+  clients.push({
+    clientName: socket.id,
+    socketId: socket.id
+  });
+  console.log("Clients: ", clients);
+
+
+  socket.on("message", (message, room) => {
+    if (room) {
+      console.log("in room")
+      // add message to chatPerClient object.
+      if (chatPerClient[`${socket.id}_to_${room}`]) {
+        console.log(`inside ${socket.id}_to_${room}`)
+        chatPerClient[`${socket.id}_to_${room}`].push({ from: socket.id, message });
+      } else if (chatPerClient[`${room}_to_${socket.id}`]) {
+        console.log(`inside ${room}_to_${socket.id}`)
+        chatPerClient[`${room}_to_${socket.id}`].push({ from: socket.id, message });
+      } else {
+        console.log(`inside else`)
+        chatPerClient[`${room}_to_${socket.id}`] = [];
+        chatPerClient[`${room}_to_${socket.id}`].push({ from: socket.id, message });
+      }
+
+      console.log("chatPerClient: ", chatPerClient);
+
+      socket.to(room).emit("message", { socketId: socket.id, message });
+    } else {
+      console.log("message");
+      io.emit("message", { socketId: socket.id, message });
+      console.log("message sent");
+    }
+  });
+
+  socket.on('join-room', (room) => {
+    socket.join(room);
+  })
+
+  socket.on('activeRoomSwitch', (room, activeRoomId) => {
+    console.log("active room switch", room, activeRoomId);
+    let chatHistory = "";
+    if (chatPerClient[`${room}_to_${activeRoomId}`]) {
+      chatHistory = chatPerClient[`${room}_to_${activeRoomId}`];
+    } else if (chatPerClient[`${activeRoomId}_to_${room}`]) {
+      chatHistory = chatPerClient[`${activeRoomId}_to_${room}`];
+    } else {
+      chatHistory = [];
+    }
+
+    console.log("chatHistory: ", chatHistory);
+    socket.to(activeRoomId).emit("activeRoomChatOnSwitch", chatHistory);
+  });
+
+  io.emit("clientList", clients);
+});
+
+server.listen(5002, () => { console.log("listening on port 8080") })
