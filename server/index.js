aa00a471e28df1a4092edd47e3363c5b9de26730
Akshay Sonwalkar <sonakashay@gmail.com>
2024-01-20 14:59:50 +0530

    feat: create room, live chat, store chat and load chat done

--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ const io = new serverSocketIo(server, {
 });
 
 const clients = [];
+const customRooms = {}; // { <roomName>: [] }
 let chatPerClient = {}; //  { <client_id>to<client_id>: [{ from: "", message: ""}]}
+const customRoomChats = {}; // { <room_id>: [{ from: "", message: ""}]}
 
 io.on("connection", (socket) => {
   console.log("connection established");
@@ -21,8 +23,22 @@ io.on("connection", (socket) => {
 
 
   socket.on("message", (message, room) => {
-    if (room) {
-      console.log("in room")
+    if (customRooms[room]) {
+      console.log("in custom room", room)
+      // add message to chatPerClient object.
+      if (chatPerClient[room]) {
+        chatPerClient[room].push({ from: socket.id, message });
+      } else {
+        console.log(`inside else`)
+        chatPerClient[room] = [];
+        chatPerClient[room].push({ from: socket.id, message });
+      }
+
+      console.log("chatPerClient: ", chatPerClient);
+
+      socket.to(room).emit("message", { socketId: socket.id, message, room });
+    } else if (room) {
+      console.log("in room", room)
       // add message to chatPerClient object.
       if (chatPerClient[`${socket.id}_to_${room}`]) {
         console.log(`inside ${socket.id}_to_${room}`)
@@ -38,7 +54,7 @@ io.on("connection", (socket) => {
 
       console.log("chatPerClient: ", chatPerClient);
 
-      socket.to(room).emit("message", { socketId: socket.id, message });
+      socket.to(room).emit("message", { socketId: socket.id, message, room });
     } else {
       console.log("message");
       io.emit("message", { socketId: socket.id, message });
@@ -48,12 +64,22 @@ io.on("connection", (socket) => {
 
   socket.on('join-room', (room) => {
     socket.join(room);
+    if (customRooms[room]) {
+      customRooms[room].push(
+        socket.id
+      );
+    } else {
+      customRooms[room] = [socket.id];
+      chatPerClient[room] = [];
+    }
   })
 
   socket.on('activeRoomSwitch', (room, activeRoomId) => {
     console.log("active room switch", room, activeRoomId);
     let chatHistory = "";
-    if (chatPerClient[`${room}_to_${activeRoomId}`]) {
+    if (customRooms[activeRoomId]) {
+      chatHistory = chatPerClient[activeRoomId];
+    } else if (chatPerClient[`${room}_to_${activeRoomId}`]) {
       chatHistory = chatPerClient[`${room}_to_${activeRoomId}`];
     } else if (chatPerClient[`${activeRoomId}_to_${room}`]) {
       chatHistory = chatPerClient[`${activeRoomId}_to_${room}`];
@@ -65,7 +91,7 @@ io.on("connection", (socket) => {
     socket.emit("activeRoomChatOnSwitch", chatHistory);
   });
 
-  io.emit("clientList", clients);
+  io.emit("clientList", clients, customRooms);
 });
 
 server.listen(5002, () => { console.log("listening on port 8080") })
