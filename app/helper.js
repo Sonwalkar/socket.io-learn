42aaa0a327e3ee4c58a8951f4263e923ba5cba8e
Akshay Sonwalkar <sonakashay@gmail.com>
2024-01-27 18:18:06 +0530

    docs: added function docs

--- app/helper.js
+++ app/helper.js
@@ -11,6 +11,13 @@ const showClientIdInHeader = (socket) => {
   document.querySelector("ul").appendChild(el);
 };
 
+/**
+ * The function removes all inactive user div elements and the active user tab from the UI, and returns
+ * the details of the removed active user tab.
+ * @returns The function `removeAllUsersAndGroupFromUI` returns an object `activeUserTabDetails` which
+ * contains the name and value of the active user tab that was removed from the DOM. If there is no
+ * active user tab, it returns an empty object.
+ */
 const removeAllUsersAndGroupFromUI = () => {
   /* The code is selecting all the inactive user div elements within the ".window .userList" container
   and removing them from the DOM. */
@@ -36,6 +43,16 @@ const removeAllUsersAndGroupFromUI = () => {
   return activeUserTabDetails;
 };
 
+/**
+ * The function `addUserOrGroupTab` creates a new div element with a specified class, text content, and
+ * data attribute, and appends it to a specific element in the DOM.
+ * @param userName - The `userName` parameter is a string that represents the name of the user or group
+ * that will be displayed in the tab.
+ * @param userId - The `userId` parameter is the unique identifier for the user or group. It is used to
+ * associate the user or group with the corresponding tab in the user list.
+ * @param tabStatus - The `tabStatus` parameter is a string that represents the status of the tab. It
+ * could be used to apply different styles or classes to the created div element based on the status.
+ */
 const addUserOrGroupTab = (userName, userId, tabStatus) => {
   const div = document.createElement("div");
   div.className = tabStatus;
@@ -44,6 +61,16 @@ const addUserOrGroupTab = (userName, userId, tabStatus) => {
   document.querySelector(".window .userList").appendChild(div);
 };
 
+/**
+ * The function "showUsersAndGroup" displays all users and groups, making them inactive except for the
+ * activeUserTabDetails.
+ * @param clients - An array of objects representing the clients connected to the server. Each object
+ * has the following properties:
+ * @param customRooms - An object that contains information about custom rooms. Each key represents a
+ * room name, and the corresponding value is an array of socket IDs of the clients in that room.
+ * @param activeUserTabDetails - activeUserTabDetails is an object that contains the details of the
+ * currently active user tab. It has the following properties:
+ */
 const showUsersAndGroup = (clients, customRooms, activeUserTabDetails) => {
   // Show all the users and make them inactive except activeUserTabDetails.
   for (const client of clients) {
